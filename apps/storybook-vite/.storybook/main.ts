069a7ae79e6eddad8a4b4b96e5c45d09914d9cec
Dominic Garms <djgarms@gmail.com>
2022-08-24 16:40:48 +0800

    further tryouts vite

--- apps/storybook-vite/.storybook/main.ts
+++ apps/storybook-vite/.storybook/main.ts
@@ -1,34 +1,16 @@
 import type {StorybookViteConfig} from '@storybook/builder-vite';
 import {tamaguiExtractPlugin, tamaguiPlugin} from '@tamagui/vite-plugin'
-import {defineConfig, mergeConfig} from 'vite'
-import {shouldExclude} from "tamagui-loader";
-import path from "path";
-const projectRoot = path.resolve(__dirname, "..")
+import {mergeConfig} from 'vite'
 
-const target = process.env.TARGET || 'css'
-
-const tamaguiOptions = {
+const tamaguiConfig = {
     config: './tamagui.config.ts',
-    components: ['tamagui', '@my/ui', 'app'],
-    importsWhitelist: ['constants.js', 'colors.js'],
+    components: [
+        /*'app', '@my/ui', '@my/config',*/ 'tamagui', '@tamagui/core'
+    ],
     logTimings: true,
+    importsWhitelist: ['constants.js', 'colors.js'],
     disableExtraction: process.env.NODE_ENV === 'development'
 }
-// const tamaguiConfig = {
-//     config: './tamagui.config.ts',
-//     components: ['tamagui', '@my/ui', 'app']
-// }
-
-// const defaultConfig = defineConfig({
-//     clearScreen: false,
-//     plugins: [
-//         tamaguiPlugin({
-//             ...tamaguiConfig,
-//             useReactNativeWebLite: false,
-//         }),
-//         tamaguiExtractPlugin(tamaguiConfig)
-//     ],
-// })
 
 const config: StorybookViteConfig = {
     stories: [
@@ -36,90 +18,39 @@ const config: StorybookViteConfig = {
         "../../../packages/app/**/*.stories.@(js|jsx|ts|tsx|mdx)",
         "../../../packages/ui/**/*.stories.@(js|jsx|ts|tsx|mdx)",
     ],
+
     addons: [
         "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        // {
-        //     name: "@storybook/addon-react-native-web",
-        //     options: {
-        //         modulesToTranspile: [
-        //             "react-native-vector-icons",
-        //             "tamagui",
-        //             "@my/ui",
-        //             "app"
-        //         ],
-        //         babelPlugins: [
-        //             // "react-native-reanimated/plugin",
-        //             [
-        //                 '@tamagui/babel-plugin',
-        //                 {
-        //                     components: ['tamagui', '@tamagui/core', '@my/ui'],
-        //                     config: './tamagui.config.ts',
-        //                 },
-        //             ],
-        //             [
-        //                 'transform-inline-environment-variables',
-        //                 {
-        //                     include: 'TAMAGUI_TARGET',
-        //                 },
-        //             ],
-        //         ],
-        //     },
-        // },
+        "@storybook/addon-essentials"
     ],
     core: {
         builder: "@storybook/builder-vite",
         disableTelemetry: true,
     },
     framework: "@storybook/react",
-    // typescript: { reactDocgen: false },
-    // webpackFinal: async (config, { configType }) => {
-    //   // config.resolve.fallback = {
-    //   //   stream: require.resolve("stream-browserify"),
-    //   //   path: require.resolve("path-browserify"),
-    //   // };
-    //   return config;
-    // },
+    features: {
+        storyStoreV7: true,
+    },
     viteFinal(config) {
-        // return mergeConfig(defaultConfig, config)
         return mergeConfig(config, {
+            plugins: [
+                tamaguiPlugin({
+                    ...tamaguiConfig,
+                    useReactNativeWebLite: false,
+                }),
+                tamaguiExtractPlugin(tamaguiConfig),
+            ],
             define: {
                 global: {},
             },
-            resolve: {
-                alias: {
-                    'react-native': 'react-native-web',
-                    'react-native/Libraries/Renderer/shims/ReactFabric': '@tamagui/proxy-worm',
-                    // 'react-native-reanimated': require.resolve('react-native-reanimated'),
-                    // 'react-native-reanimated$': require.resolve('react-native-reanimated'),
-                },
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.[jt]sx?$/,
-                        // todo
-                        exclude: path => shouldExclude(path, projectRoot),
-                        use: [
-                            // todo: get thread-loader working later
-                            // optionally thread-loader for significantly faster compile!
-                            // 'thread-loader',
-                            //
-                            {
-                                loader: 'esbuild-loader',
-                                options: {
-                                    loader: 'tsx',
-                                    minify: process.env.NODE_ENV === 'production',
-                                },
-                            },
-                            {
-                                loader: 'tamagui-loader',
-                                options:  tamaguiOptions
-                            },
-                        ]
-                    }
-                ]
-            },
+            // resolve: {
+            //     alias: {
+            //         'react-native': 'react-native-web',
+            //         'react-native/Libraries/Renderer/shims/ReactFabric': '@tamagui/proxy-worm',
+            //         // 'react-native-reanimated': require.resolve('react-native-reanimated'),
+            //         // 'react-native-reanimated$': require.resolve('react-native-reanimated'),
+            //     },
+            // }
         })
     }
 };
