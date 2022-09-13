48add868f506fa394520565e52469c89f50ae80b
Dominic Garms <djgarms@gmail.com>
2022-09-13 10:55:04 +0800

    move devtools

--- packages/app/src/provider/LmNhostProvider.web.tsx
+++ packages/app/src/provider/LmNhostProvider.web.tsx
@@ -3,12 +3,14 @@ import {NhostReactProvider} from "@nhost/react";
 import {nhost} from "app/src/lib/nhostClient";
 import {QueryClientProvider} from '@tanstack/react-query'
 import {queryClient} from "app/src/lib/queryClient";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 export function LmNhostProvider({children}: PropsWithChildren) {
     return (
         <NhostReactProvider nhost={nhost}>
             <QueryClientProvider client={queryClient}>
                 {children}
+                <ReactQueryDevtools initialIsOpen={false}/>
             </QueryClientProvider>
         </NhostReactProvider>
     )
