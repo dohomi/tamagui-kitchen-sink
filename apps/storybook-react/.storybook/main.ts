94d008665bd41a744ee80d9d886c0096e6b322b9
Dominic Garms <djgarms@gmail.com>
2022-09-21 22:00:08 +0800

    try fixing sb build

--- apps/storybook-react/.storybook/main.ts
+++ apps/storybook-react/.storybook/main.ts
@@ -13,7 +13,7 @@ const config = {
             options: {
                 modulesToTranspile: [
                     'app',
-                    '@my/ui',
+                    // '@my/ui',
                     '@my/config'
                 ],
                 babelPlugins: [
