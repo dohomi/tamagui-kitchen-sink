be7c8977ce199b318ef66c05bc585abe2c1f8bb2
Steffen Baer <stbaer.mail@gmail.com>
2022-09-21 15:57:41 +0800

    apply theme to multiselect on web

--- packages/ui/src/components/forms/LmMultiSelect.tsx
+++ packages/ui/src/components/forms/LmMultiSelect.tsx
@@ -1,18 +1,39 @@
 import { useState } from 'react';
 import Select, { Props, OptionProps } from 'react-select';
+import { useTheme } from 'tamagui';
 
 
-type LmMultiSelectProps = {} & Props
+export type LmMultiSelectProps = {} & Props
 
 export function LmMultiSelect(props: LmMultiSelectProps) {
-    const [selectedOption, setSelectedOption] = useState(null);/**/
+    const [selectedOption, setSelectedOption] = useState(null);
+    const { background, backgroundHover } = useTheme()
 
+    // console.log(background, color, borderColor)
     return (
-            <Select
-                {...props}
-                defaultValue={selectedOption}
-                onChange={setSelectedOption}
-                options={props.options}
-            />
+        <Select
+            {...props}
+            styles={{
+                container: (provided) => ({
+                    ...provided,
+                    backgroundColor: 'inherit'
+                })
+            }}
+            defaultValue={selectedOption}
+            onChange={setSelectedOption}
+            options={props.options}
+            theme={(theme) => {
+                // console.log(theme)
+                return {
+                    ...theme,
+                    // borderRadius: 0,
+                    colors: {
+                        ...theme.colors,
+                        neutral0: background.val as string,
+                        primary25: backgroundHover.val as string // <- hover highlight
+                    },
+                }
+            }}
+        />
     );
 }
--- packages/ui/stories/components/forms/LmMultiSelect.stories.tsx
+++ packages/ui/stories/components/forms/LmMultiSelect.stories.tsx
@@ -1,6 +1,6 @@
 import {ComponentMeta, ComponentStory} from "@storybook/react";
 import {XStack, YStack} from "tamagui";
-import {LmMultiSelect} from "../../../src";
+import {LmMultiSelect, LmMultiSelectProps} from "../../../src";
 import {fruitItemsFixtures} from "../../../fixtures/selectItems";
 
 export default {
@@ -13,12 +13,9 @@ const options = fruitItemsFixtures
 const Template: ComponentStory<typeof LmMultiSelect> = (args) => <LmMultiSelect {...args} />
 export const Basic = Template.bind({})
 Basic.args = {
-    options
-}
-
-export const Multiple = Template.bind({})
-Multiple.args = {
     options,
     placeholder: "Multiple",
-    isMulti: true
-}
+    isMulti: true,
+} as LmMultiSelectProps
+
+
