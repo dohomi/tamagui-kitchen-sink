1626007d41029b39a89b7d137e8ffa0c8914d9a8
Dominic Garms <djgarms@gmail.com>
2022-10-07 12:39:23 +0800

    memoize months

--- packages/ui/src/date/LmMonthsPicker.tsx
+++ packages/ui/src/date/LmMonthsPicker.tsx
@@ -1,10 +1,13 @@
 import {getMonthOptions} from "./dateHelper";
 import {LmSelect, LmSelectProps} from "../components/forms";
+import {useMemo} from "react";
 
 export type LmMonthsPickerProps = Omit<LmSelectProps, 'options'>
 
 export function LmMonthsPicker(selectProps: LmMonthsPickerProps) {
-    const names = getMonthOptions()
+    const names = useMemo(() => {
+        return getMonthOptions()
+    }, [])
     return (
         <LmSelect options={names} {...selectProps}/>
     )
