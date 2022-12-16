946c493e55804652963b4ac86040adf6b47a8fcc
Dominic Garms <djgarms@gmail.com>
2022-12-16 09:42:57 +0100

    add paddings to dialog actions

--- packages/ui/src/components/panels/LmDialogActions.tsx
+++ packages/ui/src/components/panels/LmDialogActions.tsx
@@ -4,6 +4,10 @@ export type LmDialogActionsProps = XStackProps
 
 export function LmDialogActions({children, ...rest}: LmDialogActionsProps) {
     return (
-        <XStack justifyContent={'flex-end'} alignItems={'center'} {...rest}>{children}</XStack>
+        <XStack justifyContent={'flex-end'} alignItems={'center'}
+                paddingLeft={'$4'}
+                paddingRight={'$4'}
+                paddingBottom={'$4'}
+                {...rest}>{children}</XStack>
     )
 }
