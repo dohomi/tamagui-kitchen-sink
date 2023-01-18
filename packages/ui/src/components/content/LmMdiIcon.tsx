1bf31b48475487a14ae02b28383228f4e0dc4a07
Dominic Garms <djgarms@gmail.com>
2023-01-18 10:12:20 +0800

    update tamagui

--- packages/ui/src/components/content/LmMdiIcon.tsx
+++ packages/ui/src/components/content/LmMdiIcon.tsx
@@ -1,28 +1,23 @@
-import {ColorTokens, getVariable, SizeTokens, useTheme} from "tamagui";
-import {getSize} from "@tamagui/core";
-import {Path, Svg} from "react-native-svg";
+import { ColorTokens, getVariable, SizeTokens, useTheme } from 'tamagui'
+import { getSize } from '@tamagui/get-size'
+import { Path, Svg } from 'react-native-svg'
 import type * as MDI from '@mdi/js'
 
 export type LmMdiIcon = {
-    size?: SizeTokens | number
-    color?: ColorTokens | string
-    name: keyof typeof MDI
+  size?: SizeTokens
+  color?: ColorTokens | string
+  name: keyof typeof MDI
 }
 
-export function LmMdiIcon({size, color = '$color', name}: LmMdiIcon) {
-    const theme = useTheme()
-    const calculatedColor = getVariable(
-        theme[color] || color || theme.color
-    )
-    const calculatedSize = typeof size === 'string' ? getSize(size) : (size || 24)
-    let sized = calculatedSize?.val || calculatedSize;
-    return (
-        <Svg width={sized}
-             height={sized}
-             viewBox={'0 0 24 24'}
-             fill={calculatedColor as string}>
-            <Path d={require('@mdi/js')[name]}/>
-        </Svg>
-    )
+export function LmMdiIcon({ size, color = '$color', name }: LmMdiIcon) {
+  const theme = useTheme()
+  const calculatedColor = getVariable(theme[color] || color || theme.color)
+  const calculatedSize = getSize(size)
+  // @ts-ignore
+  const sized = calculatedSize?.val || 24
+  return (
+    <Svg width={sized} height={sized} viewBox={'0 0 24 24'} fill={calculatedColor as string}>
+      <Path d={require('@mdi/js')[name]} />
+    </Svg>
+  )
 }
-
