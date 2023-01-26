8be05eedc4c1cf58151e708a9cbf700ad4bdbe61
Steffen <stbaer.mail@gmail.com>
2023-01-26 10:01:35 +0800

    fix phosphor icons / theme toggle

--- packages/app/src/provider/LmTamaguiProvider.tsx
+++ packages/app/src/provider/LmTamaguiProvider.tsx
@@ -1,4 +1,5 @@
 import {TamaguiProvider, TamaguiProviderProps} from 'tamagui'
+import {IconContextProvider} from 'tamagui-phosphor-icons'
 import config from '../../tamagui.config'
 
 type LmTamaguiProviderProps = Omit<TamaguiProviderProps, 'config'>
@@ -6,7 +7,9 @@ type LmTamaguiProviderProps = Omit<TamaguiProviderProps, 'config'>
 export function LmTamaguiProvider({children, ...rest}: LmTamaguiProviderProps) {
     return (
         <TamaguiProvider config={config} disableInjectCSS={!process.env.STORYBOOK} {...rest}>
+            <IconContextProvider value={{color: '$color'}}>
             {children}
+            </IconContextProvider>
         </TamaguiProvider>
     )
 }
