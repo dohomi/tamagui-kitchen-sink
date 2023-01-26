9eb6ea327f14c568768b4e33be451ec0f3f68d43
Dominic Garms <djgarms@gmail.com>
2023-01-26 10:32:56 +0800

    update

--- packages/app/src/provider/LmTamaguiProvider.tsx
+++ packages/app/src/provider/LmTamaguiProvider.tsx
@@ -1,15 +1,13 @@
-import {TamaguiProvider, TamaguiProviderProps} from 'tamagui'
-import {IconContextProvider} from 'tamagui-phosphor-icons'
+import { TamaguiProvider, TamaguiProviderProps } from 'tamagui'
+import { IconContextProvider } from 'tamagui-phosphor-icons'
 import config from '../../tamagui.config'
 
 type LmTamaguiProviderProps = Omit<TamaguiProviderProps, 'config'>
 
-export function LmTamaguiProvider({children, ...rest}: LmTamaguiProviderProps) {
-    return (
-        <TamaguiProvider config={config} disableInjectCSS={!process.env.STORYBOOK} {...rest}>
-            <IconContextProvider value={{color: '$color'}}>
-            {children}
-            </IconContextProvider>
-        </TamaguiProvider>
-    )
+export function LmTamaguiProvider({ children, ...rest }: LmTamaguiProviderProps) {
+  return (
+    <TamaguiProvider config={config} disableInjectCSS={!process.env.STORYBOOK} {...rest}>
+      <IconContextProvider value={{ color: '$color' }}>{children}</IconContextProvider>
+    </TamaguiProvider>
+  )
 }
