c300b561904419328150caa0daad1b3e78ebebe8
Dominic Garms <djgarms@gmail.com>
2023-02-16 11:07:06 +0800

    update tamagui theme

--- packages/app/src/state/themeState.ts
+++ packages/app/src/state/themeState.ts
@@ -1,19 +1,18 @@
-import create from "zustand";
+import { create } from 'zustand'
 
-export type ThemeVars = 'dark' | 'light';
+export type ThemeVars = 'dark' | 'light'
 type Theme = {
-    name?: ThemeVars
-    changeTheme: (name: ThemeVars) => void
-    toggleTheme: () => void
+  name?: ThemeVars
+  changeTheme: (name: ThemeVars) => void
+  toggleTheme: () => void
 }
 
-
 export const useThemeState = create<Theme>((set) => ({
-    changeTheme: (name) => set({name}),
-    toggleTheme: () => set(state => ({
-        name: state.name === 'dark' ? 'light' : 'dark'
-    }))
+  changeTheme: (name) => set({ name }),
+  toggleTheme: () =>
+    set((state) => ({
+      name: state.name === 'dark' ? 'light' : 'dark',
+    })),
 }))
 
-export const useThemeToggle = () => useThemeState(state => state.toggleTheme)
-
+export const useThemeToggle = () => useThemeState((state) => state.toggleTheme)
