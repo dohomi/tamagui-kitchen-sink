e62871dee38ab9868cc1c60a54bb678765c00e1b
Dominic Garms <djgarms@gmail.com>
2023-04-08 11:52:19 +0800

    0.12.10

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,29 +1,29 @@
 module.exports = function (api) {
-    api.cache(true)
-    return {
-        presets: [['babel-preset-expo', {jsxRuntime: 'automatic'}]],
-        plugins: [
-            'inline-dotenv',
-            // if you want reanimated support
-            // 'react-native-reanimated/plugin',
-            // fix android
-            ...(process.env.EAS_BUILD_PLATFORM === 'android'
-                ? []
-                : [
-                    [
-                        '@tamagui/babel-plugin',
-                        {
-                            components: ['tamagui-extras', 'tamagui'],
-                            config: './tamagui.config.ts',
-                        },
-                    ],
-                ]),
+  api.cache(true)
+  return {
+    presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
+    plugins: [
+      'inline-dotenv',
+      // if you want reanimated support
+      // 'react-native-reanimated/plugin',
+      // fix android
+      ...(process.env.EAS_BUILD_PLATFORM === 'android'
+        ? []
+        : [
             [
-                'transform-inline-environment-variables',
-                {
-                    include: 'TAMAGUI_TARGET',
-                },
+              '@tamagui/babel-plugin',
+              {
+                components: ['tamagui-extras', 'tamagui'],
+                config: './tamagui.config.ts',
+              },
             ],
-        ]
-    }
+          ]),
+      [
+        'transform-inline-environment-variables',
+        {
+          include: 'TAMAGUI_TARGET',
+        },
+      ],
+    ],
+  }
 }
