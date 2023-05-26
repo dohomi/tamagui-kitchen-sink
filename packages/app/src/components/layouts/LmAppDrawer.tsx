ade88300cdd51f5c8d441025fc3fe7801f0ee9ce
Dominic Garms <djgarms@gmail.com>
2023-05-26 13:15:02 +0800

    minor change on app drawer

--- packages/app/src/components/layouts/LmAppDrawer.tsx
+++ packages/app/src/components/layouts/LmAppDrawer.tsx
@@ -1,10 +1,12 @@
-import { Button, ScrollView, YStack } from 'tamagui'
+import { Button, ScrollView, XStack, YStack } from 'tamagui'
 import { useAppState } from 'app/src/state/appState'
 import React, { useState } from 'react'
 import { useOnRouteChange } from 'app/src/navigation/useOnRouteChange'
 import { useTranslation } from 'react-i18next'
-import { LmSheet } from '@tamagui-extras/core'
+import { LmButton, LmGrid, LmSheet } from '@tamagui-extras/core'
 import { useRouter } from 'solito/router'
+import { LmLinkButton } from '@tamagui-extras/link'
+import { Platform } from 'react-native'
 
 export function LmAppDrawer() {
   const { t, i18n } = useTranslation()
@@ -25,50 +27,50 @@ export function LmAppDrawer() {
     >
       <ScrollView justifyContent="center" alignItems="center">
         <YStack>
-          {/*  <LmGrid space container row={'sm'}>*/}
-          {/*    <LmGrid>*/}
-          {/*      <LmLinkButton link={{ href: '/' }}>{t('home')}</LmLinkButton>*/}
-          {/*    </LmGrid>*/}
-          {/*    <LmGrid>*/}
-          {/*      <LmLinkButton link={{ href: '/icons' }}>{t('icons')}</LmLinkButton>*/}
-          {/*    </LmGrid>*/}
-          {/*    <LmGrid>*/}
-          {/*      <LmLinkButton link={{ href: '/playground' }}>{t('playground')}</LmLinkButton>*/}
-          {/*    </LmGrid>*/}
-          {/*    <LmGrid>*/}
-          {/*      <LmLinkButton link={{ href: '/overview' }}>*/}
-          {/*        {t('overviewScreen.headline')}*/}
-          {/*      </LmLinkButton>*/}
-          {/*    </LmGrid>*/}
-          {/*    <LmGrid>*/}
-          {/*      <LmLinkButton link={{ href: '/media' }}>{t('mediaScreen.headline')}</LmLinkButton>*/}
-          {/*    </LmGrid>*/}
-          {/*  </LmGrid>*/}
-          {/*</YStack>*/}
+          <LmGrid space container row={'sm'}>
+            <LmGrid>
+              <LmLinkButton link={{ href: '/' }}>{t('home')}</LmLinkButton>
+            </LmGrid>
+            <LmGrid>
+              <LmLinkButton link={{ href: '/icons' }}>{t('icons')}</LmLinkButton>
+            </LmGrid>
+            <LmGrid>
+              <LmLinkButton link={{ href: '/playground' }}>{t('playground')}</LmLinkButton>
+            </LmGrid>
+            <LmGrid>
+              <LmLinkButton link={{ href: '/overview' }}>
+                {t('overviewScreen.headline')}
+              </LmLinkButton>
+            </LmGrid>
+            <LmGrid>
+              <LmLinkButton link={{ href: '/media' }}>{t('mediaScreen.headline')}</LmLinkButton>
+            </LmGrid>
+          </LmGrid>
+        </YStack>
 
-          {/*<YStack padding="$4" justifyContent="center" alignItems="center">*/}
-          {/*  <XStack space marginVertical={'$4'}>*/}
-          {/*    <LmButton*/}
-          {/*      onPress={() => {*/}
-          {/*        if (process.env.STORYBOOK || Platform.OS !== 'web') {*/}
-          {/*          return i18n.changeLanguage('de')*/}
-          {/*        }*/}
-          {/*        push('/', undefined, { locale: 'de' })*/}
-          {/*      }}*/}
-          {/*    >*/}
-          {/*      DE*/}
-          {/*    </LmButton>*/}
-          {/*    <LmButton*/}
-          {/*      onPress={() => {*/}
-          {/*        if (process.env.STORYBOOK || Platform.OS !== 'web') {*/}
-          {/*          return i18n.changeLanguage('en')*/}
-          {/*        }*/}
-          {/*        push('/', undefined, { locale: 'en' })*/}
-          {/*      }}*/}
-          {/*    >*/}
-          {/*      EN*/}
-          {/*    </LmButton>*/}
-          {/*  </XStack>*/}
+        <YStack padding="$4" justifyContent="center" alignItems="center">
+          <XStack space marginVertical={'$4'}>
+            <LmButton
+              onPress={() => {
+                if (process.env.STORYBOOK || Platform.OS !== 'web') {
+                  return i18n.changeLanguage('de')
+                }
+                push('/', undefined, { locale: 'de' })
+              }}
+            >
+              DE
+            </LmButton>
+            <LmButton
+              onPress={() => {
+                if (process.env.STORYBOOK || Platform.OS !== 'web') {
+                  return i18n.changeLanguage('en')
+                }
+                push('/', undefined, { locale: 'en' })
+              }}
+            >
+              EN
+            </LmButton>
+          </XStack>
           <Button onPress={() => setMainDrawer(false)}>{t('close')}</Button>
         </YStack>
       </ScrollView>
