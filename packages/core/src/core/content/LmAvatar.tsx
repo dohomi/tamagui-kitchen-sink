a0dab9cf038193d8006359bef40e5d58d230c580
Dominic Garms <djgarms@gmail.com>
2023-09-06 08:41:05 +0800

    fix image source fallback on avatar

--- packages/core/src/core/content/LmAvatar.tsx
+++ packages/core/src/core/content/LmAvatar.tsx
@@ -1,4 +1,12 @@
-import { Avatar, AvatarProps, FontSizeTokens, ImageProps, Paragraph, ParagraphProps } from 'tamagui'
+import {
+  Avatar,
+  AvatarProps,
+  FontSizeTokens,
+  ImageProps,
+  Paragraph,
+  ParagraphProps,
+  useProps,
+} from 'tamagui'
 
 export type LmAvatarProps = AvatarProps & {
   color?: AvatarProps['backgroundColor']
@@ -9,18 +17,29 @@ export type LmAvatarProps = AvatarProps & {
 }
 
 export function LmAvatar({ color, src, letter, letterProps, imageProps, ...rest }: LmAvatarProps) {
+  const p = useProps(rest)
+  console.log(p)
   return (
     <Avatar
       circular={rest.circular ?? true}
       {...rest}
       backgroundColor={src ? undefined : color || '$gray10'}
     >
-      {src ? (
+      {src || imageProps?.source ? (
         <>
-          <Avatar.Image source={{ uri: src }} {...imageProps} />
-          <Avatar.Fallback backgroundColor={color || '$gray10'} />
+          <Avatar.Image
+            {...imageProps}
+            {...(imageProps?.source
+              ? {
+                  source: imageProps.source,
+                }
+              : {
+                  src: src || imageProps?.src,
+                })}
+          />
+          <Avatar.Fallback delayMs={600} backgroundColor={color || '$gray10'} />
         </>
-      ) : (
+      ) : letter ? (
         <Paragraph
           fontSize={rest.size as FontSizeTokens}
           color={'white'}
@@ -29,6 +48,8 @@ export function LmAvatar({ color, src, letter, letterProps, imageProps, ...rest 
         >
           {letter}
         </Paragraph>
+      ) : (
+        <Avatar.Fallback backgroundColor={color || '$gray10'} />
       )}
     </Avatar>
   )
